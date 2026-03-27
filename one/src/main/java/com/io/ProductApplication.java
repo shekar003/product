4e3f96e7d55cfe26c210e780746f25a6c60b1609
shekars <shekar003@yahoo.com>
2026-03-27 21:24:43 +0530

    mine

--- one/src/main/java/com/io/ProductApplication.java
+++ one/src/main/java/com/io/ProductApplication.java
@@ -8,13 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 
  * @author Shekar
  */
-
 @SpringBootApplication(scanBasePackages = "com.io.*")
 public class ProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
-
-	} 
-
+	}
 }
