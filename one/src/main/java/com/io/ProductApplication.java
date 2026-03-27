8bcc511b498f84806e6ba201cf66287e63ff8adc
shekars <shekar003@yahoo.com>
2026-03-27 23:55:38 +0530

    merged

--- one/src/main/java/com/io/ProductApplication.java
+++ one/src/main/java/com/io/ProductApplication.java
@@ -10,5 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.io.*")
 public class ProductApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);  }
+		SpringApplication.run(ProductApplication.class, args); 
+		
+	
+	
+	}
 }
